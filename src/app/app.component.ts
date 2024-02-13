87ec6895135bf0aeb2ff96a0f72fd7afb17589bb
muhiasamuel <smuhia622@gmail.com>
2024-02-13 11:08:57 +0300

    removed unnecessary files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    console.log("app started");
+    
+  }
   title = 'TMIS';
 }
--- src/app/pages/self-assessment/self-assessment.component.ts
+++ src/app/pages/self-assessment/self-assessment.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-self-assessment',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './self-assessment.component.scss'
 })
 export class SelfAssessmentComponent {
-
+  constructor(private route:Router){}
+  clicked(){
+    this.route.navigate(['/dashboard']);
+    
+  }
 }
