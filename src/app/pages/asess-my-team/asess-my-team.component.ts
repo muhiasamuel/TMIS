f7f3a4e70e9a4b9b4bfb3bb2b943d481941bbd31
Nanakih <156086758+Nanakih@users.noreply.github.com>
2024-02-23 11:48:27 +0300

    my team

--- src/app/pages/asess-my-team/asess-my-team.component.ts
+++ src/app/pages/asess-my-team/asess-my-team.component.ts
@@ -2,9 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-asess-my-team',
-  templateUrl: './asess-my-team.component.html',
+  templateUrl:'./asess-my-team.component.html',
   styleUrl: './asess-my-team.component.scss'
 })
 export class AsessMyTeamComponent {
+  employees: any[]= [
+    { PF: '0076', name: 'Grace', Department: 'nursing', Actions: 'View'},
+    { PF: '0077', name: 'Beth', Department: 'anatomy', Actions: 'View'},
+    { PF: '0078', name: 'Mary', Department: 'microbiology', Actions: 'View'},
+    { PF: '0079', name: 'Muthui', Department: 'oncology', Actions: 'View'},
+    { PF: '0074', name: 'Victor', Department: 'paedtric', Actions: 'View'},
+    { PF: '0075', name: 'Dan', Department: 'dietetic', Actions: 'View'}
+  ]
+  toggleCard(): void{
+    this.showCard = !this.showCard; 
+  }
 
+  showCard: boolean=false;
 }
