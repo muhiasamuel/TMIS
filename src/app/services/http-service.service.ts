63b6c934826bcbb0cede306cbafed1f84d5aa4a0
muhiasamuel <smuhia622@gmail.com>
2024-03-04 12:31:21 +0300

    add critical role endpoint consumed

--- src/app/services/http-service.service.ts
+++ src/app/services/http-service.service.ts
@@ -9,7 +9,7 @@ export class HttpServiceService {
 
   constructor(private http:HttpClient) { }
 
-  serverUrl: string = 'http://192.168.90.33:8080/';
+  serverUrl: string = 'http://localhost:8080/';
  
   //add RolesFor assesssment
    data = {
@@ -23,13 +23,13 @@ export class HttpServiceService {
     "riskImpact":"5",
     "vacancyRisk":"2.5",
     "impactOnOperation":"3",
-    "skillExpirience":"4"
+    "skillExperience":"4"
    }
 
   createRoleAssessment(managerId:number,formData:any): Observable<any>{
     const url = `${this.serverUrl}addRole?managerId=${managerId}`
     const headers = new HttpHeaders({'content-type':'application/json'})
-    return this.http.post<any>(url, this.data,{headers})
+    return this.http.post<any>(url, formData)
   }
 
 }
