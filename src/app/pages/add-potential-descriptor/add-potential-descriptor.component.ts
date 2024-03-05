c01dda29b7b22aac91bf882b519d69e40a427c06
Augustine-Muthui <muthuiaugustine@gmail.com>
2024-03-05 16:21:58 +0300

    self assessment component changes

---
+++ src/app/pages/add-potential-descriptor/add-potential-descriptor.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-potential-descriptor',
+  templateUrl: './add-potential-descriptor.component.html',
+  styleUrl: './add-potential-descriptor.component.scss'
+})
+export class AddPotentialDescriptorComponent {
+
+}
---
+++ src/app/pages/asess-my-team/components/view-dialog/view-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-dialog',
+  templateUrl: './view-dialog.component.html',
+  styleUrl: './view-dialog.component.scss'
+})
+export class ViewDialogComponent {
+
+}
